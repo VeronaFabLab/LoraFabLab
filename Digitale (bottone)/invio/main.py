a6778db8deb0290de2762f11de9d5c78dc92d6f2
Federico Montagna <fedemonti00@gmail.com>
2018-01-12 18:21:27 +0100

    Buzzer + button on-board

--- Digitale (bottone)/invio/main.py
+++ Digitale (bottone)/invio/main.py
@@ -1,9 +1,10 @@
 # main.py -- put your code here!
 from network import LoRa
 import socket
-import machine
+#import machine
 import pycom
 import time
+from machine import Pin
 
 # BOTTONE - INVIO
 
@@ -12,21 +13,20 @@ lora = LoRa(mode=LoRa.LORA, frequency=868000000) # 868 MHz
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 
-
 # Ferma la pulsazione del led e lo spegne
 pycom.heartbeat(False)
 pycom.rgbled(0x000000)
 
 # Configurazione PIN
-adc = machine.ADC()
-apin = adc.channel(pin='P16') # Pin fisico G3 (vedi pinout su slack)
-
+#adc = machine.ADC()
+#apin = adc.channel(pin='P16') # Pin fisico G3 (vedi pinout su slack)
+p_in = Pin("P10", mode=Pin.IN, pull=Pin.PULL_UP)
 
 while True:
-	time.sleep(0.05) # Attesa per evitare errore "EAGAIN" -> Velocità di trasmissione troppo elevata
-	if apin() > 500: # Conversione A/D, se il valore letto (mV) è maggiore di 500 allora accende led ed inizia invio dati
-		s.send('1')
-		pycom.rgbled(0xffffff) # Led acceso bianco
-	else:
-		s.send('0')
-		pycom.rgbled(0x000000) # Led spento
+    time.sleep(0.05) # Attesa per evitare errore "EAGAIN" -> Velocità di trasmissione troppo elevata
+    if (p_in() == 0): # Conversione A/D, se il valore letto (mV) è maggiore di 500 allora accende led ed inizia invio dati
+        s.send('1')
+        pycom.rgbled(0xffffff) # Led acceso bianco
+    else:
+        s.send('0')
+        pycom.rgbled(0x000000) # Led spentos
--- Digitale (bottone)/ricezione/main.py
+++ Digitale (bottone)/ricezione/main.py
@@ -2,8 +2,13 @@ from network import LoRa
 import socket
 import time
 import pycom
+from machine import PWM
 
 # BOTTONE - RICEZIONE
+
+#Setting PWM for buzzer output
+pwm= PWM(0, frequency= 3000)
+buzzer= pwm.channel(0, pin='G9', duty_cycle=0)
 
 # Setup del socket
 lora = LoRa(mode=LoRa.LORA, frequency=868000000) # 868 MHz
@@ -17,5 +22,7 @@ pycom.rgbled(0x000000)
 while True:
     if s.recv(64) == b'1':
         pycom.rgbled(0xffffff) # Led acceso bianco
+        buzzer.duty_cycle(0.5)
     elif s.recv(64) == b'0':
         pycom.rgbled(0x000000) # Led spento
+        buzzer.duty_cycle(0)
